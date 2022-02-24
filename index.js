68563985df579f0b848e8c94fb2969631fefb8f3
Nuno Miguel <nm.albano20@gmail.com>
2022-02-24 18:24:31 +0000

    config secrets token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
+const config = require("./config/config.json");
+const secrets = require("./config/secrets.json");
 
 const prefix = "-";
 const commands = require("./scripts/commandsReader")(config.prefix);
@@ -57,4 +58,4 @@ client.on("message", (message) => {
 
  }); */
 
-client.login(config.token);
+client.login(secrets.secrets.botToken);
