d967daa035455d693dfeeb2025b7b6c380c577d3
Nuno Miguel <nm.albano20@gmail.com>
2022-02-24 20:13:26 +0000

    fix no clear

--- commands/clear.js
+++ commands/clear.js
@@ -2,9 +2,6 @@ module.exports = async (client, message) => {
     const channel = message.channel;
     const fetchmessage = await channel.fetchWebhooks();
     await channel.bulkDelete(fetchmessage);
-    //com o nome do user
-    //message.reply("Clean");
-
-    //sem nome de user
-    message.channel.send("Clean");
+    message.reply("Clean");
 }
+
--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,4 @@
 module.exports = (client, message) => {
+    //sem nome de user
     message.channel.send("Para ficares verificado no servidor clica aqui");
 }
